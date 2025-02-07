ec9dad165d9bf992b7c6d5fda1c5068cc6966e2d
Ancient23 <Ancient23@users.noreply.github.com>
2025-02-06 20:49:25 -0800

    WIP: Replicated game time interval variable in game state. Some commented code scaffolding global game view model from Game instance. for later.

--- Source/GameplayFramework/Private/DaGameStateBase.cpp
+++ Source/GameplayFramework/Private/DaGameStateBase.cpp
@@ -3,7 +3,9 @@
 
 #include "DaGameStateBase.h"
 
+#include "../../../../../../../../../../Program Files/Epic Games/UE_5.5/Engine/Plugins/Runtime/ModelViewViewModel/Source/ModelViewViewModel/Public/MVVMGameSubsystem.h"
 #include "AbilitySystem/DaAbilitySystemComponent.h"
+#include "Net/UnrealNetwork.h"
 
 ADaGameStateBase::ADaGameStateBase(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
@@ -11,11 +13,19 @@ ADaGameStateBase::ADaGameStateBase(const FObjectInitializer& ObjectInitializer)
 	AbilitySystemComponent = ObjectInitializer.CreateDefaultSubobject<UDaAbilitySystemComponent>(this, TEXT("AbilitySystemComponent"));
 	AbilitySystemComponent->SetIsReplicated(true);
 	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Mixed);
+	GameTimeInterval = 1.0f;
 }
 
 UAbilitySystemComponent* ADaGameStateBase::GetAbilitySystemComponent() const
 {
 	return AbilitySystemComponent;
+}
+
+void ADaGameStateBase::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME_CONDITION_NOTIFY(ADaGameStateBase, GameTimeInterval, COND_None, REPNOTIFY_Always);
 }
 
 void ADaGameStateBase::PostInitializeComponents()
@@ -24,4 +34,13 @@ void ADaGameStateBase::PostInitializeComponents()
 
 	check(AbilitySystemComponent);
 	AbilitySystemComponent->InitAbilityActorInfo(/*Owner=*/ this, /*Avatar=*/ this);
+
+	// if (UGameInstance* GameInstance = GetGameInstance())
+	// {
+	// 	if (UMVVMGameSubsystem* ModelViewSubsystem = UGameInstance::GetSubsystem<UMVVMGameSubsystem>(GameInstance))
+	// 	{
+	// 		// Setup any global Data for UI to display here, such as list of players etc.
+	// 		
+	// 	}
+	// }
 }
